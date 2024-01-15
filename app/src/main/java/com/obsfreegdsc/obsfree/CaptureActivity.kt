4e86f09c841f98490580a878cb84622ea39075d6
adecay <younghyeon55@gmail.com>
2024-01-16 03:22:47 +0900

    feat: camera preview

--- app/src/main/java/com/obsfreegdsc/obsfree/CaptureActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/CaptureActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.Manifest
 import android.content.pm.PackageManager
+import android.util.Log
 import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.obsfreegdsc.obsfree.databinding.ActivityCaptureBinding
@@ -18,11 +22,42 @@ class CaptureActivity : AppCompatActivity() {
         setContentView(viewBinding.root)
 
         if (cameraPermissionGranted()) {
-
+            startCamera()
         } else {
             ActivityCompat.requestPermissions(
                 this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_PERMISSIONS)
         }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewBinding.prvvCaptureRearcam.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun cameraPermissionGranted() = ContextCompat.checkSelfPermission(
@@ -36,7 +71,7 @@ class CaptureActivity : AppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == REQUEST_CODE_PERMISSIONS) {
             if (cameraPermissionGranted()) {
-
+                startCamera()
             } else {
                 Toast.makeText(this,
                     "Permissions not granted by the user.",
@@ -47,6 +82,7 @@ class CaptureActivity : AppCompatActivity() {
     }
 
     companion object {
+        private const val TAG = "ObsFree"
         private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
