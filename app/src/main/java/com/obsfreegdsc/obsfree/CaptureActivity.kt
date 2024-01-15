3b2e173ecb998b6ee3cbd512db7788f58e29befc
adecay <younghyeon55@gmail.com>
2024-01-16 03:01:20 +0900

    feat: camera permission request

--- app/src/main/java/com/obsfreegdsc/obsfree/CaptureActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/CaptureActivity.kt
@@ -2,12 +2,51 @@ package com.obsfreegdsc.obsfree
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.obsfreegdsc.obsfree.databinding.ActivityCaptureBinding
 
 class CaptureActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityCaptureBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_capture)
+        viewBinding = ActivityCaptureBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        if (cameraPermissionGranted()) {
+
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_PERMISSIONS)
+        }
     }
 
+    private fun cameraPermissionGranted() = ContextCompat.checkSelfPermission(
+        baseContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (cameraPermissionGranted()) {
+
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
 }
