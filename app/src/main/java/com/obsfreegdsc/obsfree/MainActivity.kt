49a60b84617c4c99694e759023096e0739472585
adecay <younghyeon55@gmail.com>
2024-01-16 02:00:25 +0900

    feat: intent main activity to capture activity

--- app/src/main/java/com/obsfreegdsc/obsfree/MainActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.obsfreegdsc.obsfree
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.obsfreegdsc.obsfree.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.btnMainIntentcapture.setOnClickListener{ intentCapture() }
+    }
+
+    private fun intentCapture() {
+        intent = Intent(this, CaptureActivity::class.java)
+        startActivity(intent)
     }
 }
