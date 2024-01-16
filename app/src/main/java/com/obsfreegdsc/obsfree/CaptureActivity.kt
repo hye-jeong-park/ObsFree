87a22dbd97ce4f1c24d3b396d6a9b582f7b8568e
adecay <younghyeon55@gmail.com>
2024-01-16 23:12:04 +0900

    feat: save photo in internal cache directory

--- app/src/main/java/com/obsfreegdsc/obsfree/CaptureActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/CaptureActivity.kt
@@ -3,18 +3,26 @@ package com.obsfreegdsc.obsfree
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.util.Log
 import android.widget.Toast
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.obsfreegdsc.obsfree.databinding.ActivityCaptureBinding
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class CaptureActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityCaptureBinding
+
+    private var imageCapture: ImageCapture? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,8 @@ class CaptureActivity : AppCompatActivity() {
             ActivityCompat.requestPermissions(
                 this, arrayOf(Manifest.permission.CAMERA), REQUEST_CODE_PERMISSIONS)
         }
+
+        viewBinding.btnCaptureCapture.setOnClickListener{ takePhoto() }
     }
 
     private fun startCamera() {
@@ -43,6 +53,10 @@ class CaptureActivity : AppCompatActivity() {
                     it.setSurfaceProvider(viewBinding.prvvCaptureRearcam.surfaceProvider)
                 }
 
+            // ImageCapture
+            imageCapture = ImageCapture.Builder()
+                .build()
+
             // Select back camera as a default
             val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
@@ -52,12 +66,52 @@ class CaptureActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview)
+                    this, cameraSelector, preview, imageCapture)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
         }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.getDefault())
+            .format(System.currentTimeMillis())
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(File(applicationContext.cacheDir, name))
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
+            ContextCompat.getMainExecutor(this),
+            object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exception: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exception.message}", exception)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+
+                    intentConfirmReport(name)
+                }
+            }
+        )
+    }
+
+    private fun intentConfirmReport(name: String) {
+        intent = Intent(this, ConfirmReportActivity::class.java)
+        intent.putExtra("FILE_NAME", name)
+        startActivity(intent)
     }
 
     private fun cameraPermissionGranted() = ContextCompat.checkSelfPermission(
@@ -83,6 +137,7 @@ class CaptureActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "ObsFree"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
         private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
