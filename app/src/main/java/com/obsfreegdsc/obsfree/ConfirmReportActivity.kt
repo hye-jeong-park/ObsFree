855a58aaecf350cfbcac6129603566cbd55c468d
adecay <younghyeon55@gmail.com>
2024-01-17 01:50:21 +0900

    feat: delete cache file after report

--- app/src/main/java/com/obsfreegdsc/obsfree/ConfirmReportActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/ConfirmReportActivity.kt
@@ -9,14 +9,27 @@ import java.io.File
 class ConfirmReportActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityConfirmreportBinding
 
+    private var cacheFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding = ActivityConfirmreportBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
         val name = intent.getStringExtra("FILE_NAME") as String
-        val cacheFile = File(applicationContext.cacheDir, name)
-        val bitmap = BitmapFactory.decodeFile(cacheFile.absolutePath)
+        cacheFile = File(applicationContext.cacheDir, name)
+        val bitmap = BitmapFactory.decodeFile(cacheFile!!.absolutePath)
         viewBinding.imgConfirmreportPhoto.setImageBitmap(bitmap)
+
+        viewBinding.btnComfirmreportCancel.setOnClickListener{ cancel() }
+        viewBinding.btnConfirmreportReport.setOnClickListener{ report() }
+    }
+
+    private fun cancel() {
+
+    }
+
+    private fun report() {
+        cacheFile!!.delete()
     }
 }
