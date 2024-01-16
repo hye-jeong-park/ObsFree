bca4b08c49c6af069291fe1df4e1316ce2d424ac
adecay <younghyeon55@gmail.com>
2024-01-16 23:57:53 +0900

    feat: show saved image in ConfirmReportActivity

--- app/src/main/java/com/obsfreegdsc/obsfree/ConfirmReportActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/ConfirmReportActivity.kt
@@ -1,11 +1,22 @@
 package com.obsfreegdsc.obsfree
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.obsfreegdsc.obsfree.databinding.ActivityConfirmreportBinding
+import java.io.File
 
 class ConfirmReportActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityConfirmreportBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_confirmreport)
+        viewBinding = ActivityConfirmreportBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        val name = intent.getStringExtra("FILE_NAME") as String
+        val cacheFile = File(applicationContext.cacheDir, name)
+        val bitmap = BitmapFactory.decodeFile(cacheFile.absolutePath)
+        viewBinding.imgConfirmreportPhoto.setImageBitmap(bitmap)
     }
 }
