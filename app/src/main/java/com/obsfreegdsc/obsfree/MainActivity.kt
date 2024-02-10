3a0083ea5d4a977c1ea7368fbf2a17e247eb4740
adecay <younghyeon55@gmail.com>
2024-02-11 08:40:27 +0900

    feat: request location permission for alert

--- app/src/main/java/com/obsfreegdsc/obsfree/MainActivity.kt
+++ app/src/main/java/com/obsfreegdsc/obsfree/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.obsfreegdsc.obsfree
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
@@ -40,14 +47,22 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (isChecked) {
-            val workRequest =
-                OneTimeWorkRequestBuilder<AlertWorker>()
-                    .addTag("alert")
-                    .build()
-
-            WorkManager
-                .getInstance(applicationContext)
-                .enqueue(workRequest)
+            if (!foregroundLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    FOREGROUND_REQUIRED_PERMISSIONS,
+                    FOREGROUND_REQUEST_CODE_PERMISSIONS
+                )
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                !backgroundLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                    FOREGROUND_REQUEST_CODE_PERMISSIONS
+                )
+            } else {
+                scheduleAlertWorker()
+            }
         } else {
             WorkManager
                 .getInstance(applicationContext)
@@ -55,9 +70,74 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun scheduleAlertWorker() {
+        val workRequest =
+            OneTimeWorkRequestBuilder<AlertWorker>()
+                .addTag("alert")
+                .build()
+
+        WorkManager
+            .getInstance(applicationContext)
+            .enqueue(workRequest)
+    }
+
     private fun intentCapture() {
         intent = Intent(this, CaptureActivity::class.java)
         startActivity(intent)
+    }
+
+    private fun foregroundLocationPermissionsGranted() = FOREGROUND_REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private fun backgroundLocationPermissionsGranted() = ContextCompat.checkSelfPermission(
+        baseContext, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == FOREGROUND_REQUEST_CODE_PERMISSIONS) {
+            if (!foregroundLocationPermissionsGranted()) {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                viewBinding.swMainAlert.isChecked = false
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q &&
+                !backgroundLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                    BACKGROUND_REQUEST_CODE_PERMISSIONS
+                )
+            } else {
+                scheduleAlertWorker()
+            }
+        } else if (requestCode == BACKGROUND_REQUEST_CODE_PERMISSIONS) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+                && backgroundLocationPermissionsGranted()) {
+                scheduleAlertWorker()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                viewBinding.swMainAlert.isChecked = false
+            }
+        }
+    }
+
+    companion object {
+        private const val FOREGROUND_REQUEST_CODE_PERMISSIONS = 30
+        private val FOREGROUND_REQUIRED_PERMISSIONS =
+            mutableListOf (
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ).toTypedArray()
+        private const val BACKGROUND_REQUEST_CODE_PERMISSIONS = 40
     }
 }
 
